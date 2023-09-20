7a6578a51e8c191290cc61c4db0abdd71ec76bdd
pranav k <pranavkkeloth@gmail.com>
2023-09-20 18:56:04 +0530

    feat: http protocol upgraded to ws

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-
 const http = require("http")
 const express = require("express");
 const path = require("path")
+const {Server} = require("socket.io")
 
 const app = express();
 const server = http.createServer(app)
+const io = new Server(server)
 
 app.use(express.static(path.resolve("./public")))
+
+//socket.io
+io.on('connection', (socket) => {
+    console.log("A new user has connected", socket.id)
+    
+})
 
 app.get('/', (req,res)=>{
     return res.sendFile("/public/index.html")

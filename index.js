da86217023f3ef863e29e8e241bbfd4e75dcd3fd
pranav k <pranavkkeloth@gmail.com>
2023-09-20 17:55:28 +0530

    feat: Implement the server req

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
 const http = require("http")
 const express = require("express");
+const path = require("path")
 
 const app = express();
 const server = http.createServer(app)
 
-app.use(express.static('/public'))
+app.use(express.static(path.resolve("./public")))
+
+app.get('/', (req,res)=>{
+    return res.sendFile("/public/index.html")
+})
 
 server.listen(9000, ()=>console.log("Server Started at 9000 port"))
